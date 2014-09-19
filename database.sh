59cc0e20d223aca9f61e478936bc1032b6ab958a
Arthur Corenzan <arthur@corenzan.com>
2014-09-19 20:30:25 -0300

    Fix su for sudo

--- database.sh
+++ database.sh
@@ -6,4 +6,4 @@
 apt-get install -y postgresql postgresql-contrib redis-server redis-tools
 
 # Create PostgreSQL role for the application
-su -u postgres createuser -d $YOUR_USERNAME
+sudo -u postgres createuser -d $YOUR_USERNAME
