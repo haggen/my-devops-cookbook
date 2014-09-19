c8a550224335143e2c90580aa823dee8a39b35c5
Arthur Corenzan <arthur@corenzan.com>
2014-09-19 18:53:17 -0300

    Add Postgre and Redis setup

--- database.sh
+++ database.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-[[ "$0" == "database.sh" ]] && echo "This script cannot be called directly, use `setup.sh`" && exit 1
+[[ "$0" == "database.sh" ]] && echo "This script cannot be called directly, use 'setup.sh'" && exit 1
 
 # Install databases
 apt-get install -y postgresql postgresql-contrib redis-server redis-tools
 
-# Configure PostgreSQL credentials
-# ...
+# Create PostgreSQL role for the application
+su -u postgres createuser -d $YOUR_USERNAME
